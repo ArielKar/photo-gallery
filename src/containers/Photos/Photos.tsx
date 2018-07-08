d99a990885c0de09f1df599f6b94a39f679d4413
ArielK <arielkarmazin@gmail.com>
2018-07-08 14:34:59 +0300

    better UI

--- src/containers/Photos/Photos.tsx
+++ src/containers/Photos/Photos.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {connect} from 'react-redux';
 import './Photos.css';
+
 import AddPhoto from "../../components/AddPhoto/AddPhoto";
 import Photo from "../../components/Photo/Photo";
 
